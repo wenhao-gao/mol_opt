ed34ff5f37dd89a71185cb473f5d12105a08a1dc
futianfan <futianfan@gmail.com>
2022-03-05 23:56:01 -0500

    selfies ga unfinished; just save

--- main/selfies_GA/README.md
+++ main/selfies_GA/README.md
@@ -1,14 +1,9 @@
-# Augmenting genetic algorithms with deep neural networks for exploring the chemical space
+# SELFIES-GA+D: Augmenting genetic algorithms with deep neural networks for exploring the chemical space
 This repository contains code for the paper: [Augmenting genetic algorithms with deep neural networks for exploring the chemical space](https://arxiv.org/abs/1909.11655). 
 
-A video summary of the paper can be found here: https://www.youtube.com/watch?v=9VilhlEXm9w&t=16s
 
-Here is a visualization of molecular progress: 
-
-<img align="center" src="./readme_docs/mol_view.gif"/>
 
 ## Prerequisites
-For cloning the repository, please have a look at the Branch Navigator section.  
 
 Before running the code, please ensure you have the following:
 - [SELFIES (any version)](https://github.com/aspuru-guzik-group/selfies) - 
@@ -19,14 +14,9 @@ Before running the code, please ensure you have the following:
 - [Python 3.0 or up](https://www.python.org/download/releases/3.0/)
 - [numpy](https://pypi.org/project/numpy/)
 
-Please note: that the Synthetic Accesability calculater (i.e. directory SAS_calculator) comes from - [ https://github.com/EricTing/SAscore]( https://github.com/EricTing/SAscore).
 
 
-## How to run the code? : 
-We highly recommend using the following version for running your experiments.  
-```
-python ./core_GA.py
-```  
+## run the code
 
 The following settings can be customized (found at the end of the file 'core_GA.py'): 
 - num_generations: Number of generations to run the GA
@@ -43,49 +33,21 @@ The following settings can be customized (found at the end of the file 'core_GA.
 - beta: Value of parameter beta
 - impose_time_adapted_pen: Boolean variable to indicated use of a time-adapted discriminator penalty
 
-## How are the results saved?  : 
-All the results are savents in the 'results' directory. Our results are saved as (Note: 'i' is the run iteration): 
-1. images_generation_0_i:  
-   Images of the top 100 molecules of each generation. Below each molecule are the Fitness, logP, SA, ring penalty and discriminator scores
-2. results_0_i:  
-   Each sub-directory is named by the generation. The smile strings (ordered by fitness) and corresponding molecular properties are provided as text
-   files: 'smiles_ordered.txt', 'logP_ordered.txt', 'sas_ordered.txt', 'ringP_ordered.txt', 'discrP_ordered.txt'. 
-   Outside the sub-directories is the information about the best molecules of a generation. 
-3. saved_models_0_i:  
-   The trained discriminators after each generation. Please Note: We did not make use of the discriminator predictions in the Fitness for this experiment (beta is set to 0).
-
-
-## Branch Navigator: 
-The code for this repository is arranged based on the experiments of the paper. Particularly: 
-The code for the paper (arranged by experiment) can be found in the [paper_results branch](https://github.com/akshat998/GA/tree/paper_results). The experiments are arranged as follows: 
-
-- [Experiment 4.1: ](https://github.com/akshat998/GA/tree/paper_results/4.1) Unconstrained optimization and comparison with other generative models
-- [Experiment 4.2: ](https://github.com/akshat998/GA/tree/paper_results/4.2) Long term experiment with a time-dependent adaptive penalty
-- [Experiment 4.3: ](https://github.com/akshat998/GA/tree/paper_results/4.3) Analysis of molecule classes explored by the GA
-- [Experiment 4.4: ](https://github.com/akshat998/GA/tree/paper_results/4.4) Constrained optimization
-- [Experiment 4.5: ](https://github.com/akshat998/GA/tree/paper_results/4.5) Simultaneous logP and QED optimization
-- [Experiment 4.6: ](https://github.com/akshat998/GA/tree/paper_results/4.6) Modification of the hyperparameter beta
-
-Instructions on running the experiments of the paper are provided in the above links. Please note that the code has been parallelized based on the number of CPU cores for quick property evaluations.
-
-To run the code quickly, we recommend the following command: 
-```
-git clone -b master --single-branch https://github.com/aspuru-guzik-group/GA.git --depth 1
-```
-This contains the raw GA code, without any results from the paper. Above is very quick for cloning, and has a small file size.  
-
-Due to the large size of the repository, we have created a seperate branch that contains outputs from all the eperiment. For this option, please run (note: this is a 4GB branch, and needs 20mins of cloning time): 
-```
-git clone --single-branch --branch paper_results https://github.com/akshat998/GA.git
-```
 
 
 
-## Questions, problems?
-Make a github issue 😄. Please be as clear and descriptive as possible. Please feel free to reach
-out in person: (akshat[DOT]nigam[AT]mail[DOT]utoronto[DOT]ca & pascal[DOT]friederich[AT]kit[DOT]edu)
 
-## License
 
-[Apache License 2.0](https://choosealicense.com/licenses/apache-2.0/)
+
+
+
+
+
+
+
+
+
+
+
+
 
--- main/selfies_GA/run.py
+++ main/selfies_GA/run.py
@@ -1,4 +1,8 @@
 import os, time
+import sys
+path_here = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(path_here)
+sys.path.append('.')
 from selfies import decoder, encoder 
 import multiprocessing
 import torch
@@ -9,155 +13,168 @@ import generation_props as gen_func
 import numpy as np 
 
 
+from main.optimizer import BaseOptimizer
+from chemutils import * 
+from inference_utils import * 
 from tdc import Oracle
-from tdc import Evaluator
-jnk = Oracle(name = 'JNK3')
-gsk = Oracle(name = 'GSK3B')
-qed = Oracle(name = 'qed')
-from sa import sa
-def oracle(smiles):
-    try:
-        scores = [qed(smiles), sa(smiles), jnk(smiles), gsk(smiles)]
-    except:
-        # return -np.inf
-        return -100.0 
-    return np.mean(scores)
-
-global f_cache 
-f_cache = dict()
 
 
+class SELFIES_GA_optimizer(BaseOptimizer):
 
+    def __init__(self, args=None):
+        super().__init__(args)
+        self.model_name = "SELFIES_GA"
 
-def initiate_ga(num_generations,            generation_size,    starting_selfies,max_molecules_len,
-                disc_epochs_per_generation, disc_enc_type,      disc_layers,     training_start_gen,           
-                device,                     properties_calc_ls, 
-                oracle,     max_oracle_call, 
-                num_processors,  beta, max_fitness_collector, impose_time_adapted_pen):
-    
-    # Obtain starting molecule
-    starting_smiles = evo.sanitize_multiple_smiles([decoder(selfie) for selfie in starting_selfies])
-    global f_cache 
-    
-    # Recording Collective results
-    smiles_all         = []    # all SMILES seen in all generations
-    selfies_all        = []    # all SELFIES seen in all generation
-    smiles_all_counter = {}    # Number of times a SMILE string is recorded in GA run
-    
-    
-    # Initialize a Discriminator
-    discriminator, d_optimizer, d_loss_func = D.obtain_initial_discriminator(disc_enc_type, disc_layers, max_molecules_len, device)
-    
-    # Read in the Zinc data set 
-    molecules_reference = evo.read_dataset_encoding(disc_enc_type)
+    def _optimize(self, oracle, config):
 
-    molecules_reference = dict.fromkeys(molecules_reference, '') # convert the zinc data set into a dictionary
+        beta_preference = [0]
+        num_iterations  = 1
+        max_oracle_call = 200
 
-    # Set up Generation Loop 
-    total_time = time.time()
-    for generation_index in range(1, num_generations+1):
-        print("   ###   On generation %i of %i"%(generation_index, num_generations))
-        start_time = time.time()
+        results_dir = evo.make_clean_results_dir()
+                
+        beta = beta_preference[0]
+        i = 0 
+        max_fitness_collector = []
+        image_dir, saved_models_dir, data_dir = evo.make_clean_directories(beta, results_dir, i) # clear directories 
 
-              
-        # Obtain molecules from the previous generation 
-        smiles_here, selfies_here = gen_func.obtain_previous_gen_mol(starting_smiles,   starting_selfies, generation_size, 
-                                                                     generation_index,  selfies_all,      smiles_all)
+        # Initialize new TensorBoard writers
+        torch.cuda.empty_cache()
+        writer = SummaryWriter()  
 
-        # Calculate fitness of previous generation (shape: (generation_size, ))
-        fitness_here, order, fitness_ordered, smiles_ordered, selfies_ordered = gen_func.obtain_fitness(disc_enc_type,      smiles_here,   selfies_here, 
-                                                                                                        properties_calc_ls, 
-                                                                                                        oracle,        f_cache,     max_oracle_call,  
-                                                                                                        discriminator, generation_index,
-                                                                                                        max_molecules_len,  device,        generation_size,  
-                                                                                                        num_processors,     writer,        beta,            
-                                                                                                        image_dir,          data_dir,      max_fitness_collector, impose_time_adapted_pen)
-        if len(f_cache) > max_oracle_call:
-            return f_cache 
-
-        # Obtain molecules that need to be replaced & kept
-        to_replace, to_keep = gen_func.apply_generation_cutoff(order, generation_size)
-        
-        # Obtain new generation of molecules 
-        smiles_mutated, selfies_mutated = gen_func.obtain_next_gen_molecules(order,           to_replace,     to_keep, 
-                                                                             selfies_ordered, smiles_ordered, max_molecules_len)
-        # Record in collective list of molecules 
-        smiles_all, selfies_all, smiles_all_counter = gen_func.update_gen_res(smiles_all, smiles_mutated, selfies_all, selfies_mutated, smiles_all_counter)
+        num_generations = 1000
+        generation_size = 500
+        starting_selfies = [encoder('C')]
+        max_molecules_len = 81
+        disc_epochs_per_generation = 10
+        disc_enc_type = 'properties_rdkit'    # 'selfies' or 'smiles' or 'properties_rdkit'
+        disc_layers = [100, 10],
+        training_start_gen = 0   # generation index to start training discriminator
+        device = 'cpu'
+        properties_calc_ls = ['logP', 'SAS', 'RingP']   # None: No properties ; 'logP', 'SAS', 'RingP', 'QED'
+        oracle = oracle
+        max_oracle_call = max_oracle_call 
+        num_processors = multiprocessing.cpu_count()
+        beta = beta
+        max_fitness_collector = max_fitness_collector
+        impose_time_adapted_pen = True
 
         
-        # Obtain data for training the discriminator (Note: data is shuffled)
-        dataset_x, dataset_y = gen_func.obtain_discrm_data(disc_enc_type, molecules_reference, smiles_mutated, selfies_mutated, max_molecules_len, num_processors, generation_index)
-        # Train the discriminator (on mutated molecules)
-        if generation_index >= training_start_gen:
-            discriminator = D.do_x_training_steps(dataset_x, dataset_y, discriminator, d_optimizer, d_loss_func , disc_epochs_per_generation, generation_index-1, device, writer, data_dir)
-            D.save_model(discriminator, generation_index-1, saved_models_dir) # Save the discriminator 
-
-        print('Generation time: ', round((time.time()-start_time), 2), ' seconds')
-
-    print('Total time: ', round((time.time()-total_time)/60, 2), ' mins')
-    print('Total number of unique molecules: ', len(smiles_all_counter))
-    return f_cache 
-    # return smiles_all_counter
-
-
-
-if __name__ == '__main__':
+        # Obtain starting molecule
+        starting_smiles = evo.sanitize_multiple_smiles([decoder(selfie) for selfie in starting_selfies])
+        global f_cache 
         
-    beta_preference = [0] 
-    num_iterations  = 1
-    max_oracle_call = 200
+        # Recording Collective results
+        smiles_all         = []    # all SMILES seen in all generations
+        selfies_all        = []    # all SELFIES seen in all generation
+        smiles_all_counter = {}    # Number of times a SMILE string is recorded in GA run
+        
+        
+        # Initialize a Discriminator
+        discriminator, d_optimizer, d_loss_func = D.obtain_initial_discriminator(disc_enc_type, disc_layers, max_molecules_len, device)
+        
+        # Read in the Zinc data set 
+        molecules_reference = evo.read_dataset_encoding(disc_enc_type)
 
-    results_dir = evo.make_clean_results_dir()
-    
-    exper_time = time.time()
-    # for i in range(num_iterations):
-    #     for beta in beta_preference:
+        molecules_reference = dict.fromkeys(molecules_reference, '') # convert the zinc data set into a dictionary
+
+        # Set up Generation Loop 
+        total_time = time.time()
+        for generation_index in range(1, num_generations+1):
+            print("   ###   On generation %i of %i"%(generation_index, num_generations))
+            start_time = time.time()
+
+                  
+            # Obtain molecules from the previous generation 
+            smiles_here, selfies_here = gen_func.obtain_previous_gen_mol(starting_smiles,   starting_selfies, generation_size, 
+                                                                         generation_index,  selfies_all,      smiles_all)
+
+            # Calculate fitness of previous generation (shape: (generation_size, ))
+            fitness_here, order, fitness_ordered, smiles_ordered, selfies_ordered = gen_func.obtain_fitness(disc_enc_type,      smiles_here,   selfies_here, 
+                                                                                                            properties_calc_ls, 
+                                                                                                            oracle,        f_cache,     max_oracle_call,  
+                                                                                                            discriminator, generation_index,
+                                                                                                            max_molecules_len,  device,        generation_size,  
+                                                                                                            num_processors,     writer,        beta,            
+                                                                                                            image_dir,          data_dir,      max_fitness_collector, impose_time_adapted_pen)
+            if len(f_cache) > max_oracle_call:
+                return f_cache 
+
+            # Obtain molecules that need to be replaced & kept
+            to_replace, to_keep = gen_func.apply_generation_cutoff(order, generation_size)
             
-    beta = beta_preference[0]
-    i = 0 
-    max_fitness_collector = []
-    image_dir, saved_models_dir, data_dir = evo.make_clean_directories(beta, results_dir, i) # clear directories 
-            
-    # Initialize new TensorBoard writers
-    torch.cuda.empty_cache()
-    writer = SummaryWriter()  
+            # Obtain new generation of molecules 
+            smiles_mutated, selfies_mutated = gen_func.obtain_next_gen_molecules(order,           to_replace,     to_keep, 
+                                                                                 selfies_ordered, smiles_ordered, max_molecules_len)
+            # Record in collective list of molecules 
+            smiles_all, selfies_all, smiles_all_counter = gen_func.update_gen_res(smiles_all, smiles_mutated, selfies_all, selfies_mutated, smiles_all_counter)
 
-    f_cache = initiate_ga(    num_generations            = 1000,
-                                                 generation_size            = 500,
-                                                 starting_selfies           = [encoder('C')],
-                                                 max_molecules_len          = 81,
-                                                 disc_epochs_per_generation = 10,
-                                                 disc_enc_type              = 'properties_rdkit',         # 'selfies' or 'smiles' or 'properties_rdkit'
-                                                 disc_layers                = [100, 10],
-                                                 training_start_gen         = 0,                          # generation index to start training discriminator
-                                                 device                     = 'cpu',
-                                                 properties_calc_ls         = ['logP', 'SAS', 'RingP'],   # None: No properties ; 'logP', 'SAS', 'RingP', 'QED'
-                                                 oracle = oracle, 
-                                                 max_oracle_call = max_oracle_call, 
-                                                 num_processors             = multiprocessing.cpu_count(),
-                                                 beta                       = beta,
-                                                 max_fitness_collector      = max_fitness_collector,
-                                                 impose_time_adapted_pen    = True
-                                            )
 
-    # Evaluate 
-    new_score_tuples = [(v, k) for k, v in f_cache.items() if k is not None and k!='']  # scores of new molecules
-    print(new_score_tuples)
-    new_score_tuples.sort(reverse=True,key=lambda x:x[0])
-    top100_mols = [(k, v) for (v, k) in new_score_tuples[:100]]
-    diversity = Evaluator(name = 'Diversity')
-    div = diversity([t[0] for t in top100_mols])
-    output = dict(
-        top_mols=top100_mols,
-        AST=np.average([t[1] for t in top100_mols]),
-        diversity=div,
-        all_func_evals=dict(f_cache),
-    )
+            # Obtain data for training the discriminator (Note: data is shuffled)
+            dataset_x, dataset_y = gen_func.obtain_discrm_data(disc_enc_type, molecules_reference, smiles_mutated, selfies_mutated, max_molecules_len, num_processors, generation_index)
+            # Train the discriminator (on mutated molecules)
+            if generation_index >= training_start_gen:
+                discriminator = D.do_x_training_steps(dataset_x, dataset_y, discriminator, d_optimizer, d_loss_func , disc_epochs_per_generation, generation_index-1, device, writer, data_dir)
+                D.save_model(discriminator, generation_index-1, saved_models_dir) # Save the discriminator 
 
-    print('Total Experiment time: ', (time.time()-exper_time)/60, ' mins')
+            print('Generation time: ', round((time.time()-start_time), 2), ' seconds')
 
-        
-        
+        print('Total time: ', round((time.time()-total_time)/60, 2), ' mins')
+        print('Total number of unique molecules: ', len(smiles_all_counter))
+        # return smiles_all_counter
 
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--smi_file', default=None)
+    parser.add_argument('--config_default', default='hparams_default.yaml')
+    parser.add_argument('--config_tune', default='hparams_tune.yaml')
+    parser.add_argument('--n_jobs', type=int, default=-1)
+    parser.add_argument('--output_dir', type=str, default=None)
+    parser.add_argument('--patience', type=int, default=5)
+    parser.add_argument('--n_runs', type=int, default=5)
+    parser.add_argument('--task', type=str, default="simple", choices=["tune", "simple", "production"])
+    parser.add_argument('--oracles', nargs="+", default=["QED"])
+    args = parser.parse_args()
+
+    path_here = os.path.dirname(os.path.realpath(__file__))
+
+    if args.output_dir is None:
+        args.output_dir = os.path.join(path_here, "results")
+    
+    if not os.path.exists(args.output_dir):
+        os.mkdir(args.output_dir)
+    
+    for oracle_name in args.oracles:
+
+        try:
+            config_default = yaml.safe_load(open(args.config_default))
+        except:
+            config_default = yaml.safe_load(open(os.path.join(path_here, args.config_default)))
+
+        if args.task == "tune":
+            try:
+                config_tune = yaml.safe_load(open(args.config_tune))
+            except:
+                config_tune = yaml.safe_load(open(os.path.join(path_here, args.config_tune)))
+
+        oracle = Oracle(name = oracle_name)
+        optimizer = SELFIES_GA_optimizer(args=args)
+
+        if args.task == "simple":
+            optimizer.optimize(oracle=oracle, config=config_default)
+        elif args.task == "tune":
+            optimizer.hparam_tune(oracle=oracle, hparam_space=config_tune, hparam_default=config_default, count=args.n_runs)
+        elif args.task == "production":
+            optimizer.production(oracle=oracle, config=config_default, num_runs=args.n_runs)
+
+
+
+
+
+
+if __name__ == "__main__":
+    main() 
 
         
