fe6eeef31f0dfacd0e0283bf4662ccdd30e6d78e
wenhao-gao <gaowh19@gmail.com>
2022-04-25 14:31:30 -0400

    update molgpu02; optimal hparams

--- tune.sh
+++ tune.sh
@@ -1,8 +1,12 @@
 #!/bin/bash 
 
-model="graph_mcts"
+model="smiles_ga"
 
 nohup python -u run.py ${model} \
-    --n_jobs 16 --task tune --n_runs 30 --wandb online \
+    --n_jobs 16 --task tune --wandb online \
     --oracles zaleplon_mpo perindopril_mpo > tune_${model}.out 2>&1 &
+
+# nohup python -u run.py ${model} \
+#     --n_jobs 16 --task tune --n_runs 30 --wandb online \
+#     --oracles zaleplon_mpo perindopril_mpo > tune_${model}.out 2>&1 &
 
