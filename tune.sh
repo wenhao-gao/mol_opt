339154f5c3e7dc9e0c5f4d3d8f5519064d689a28
wenhao-gao <gaowh19@gmail.com>
2022-04-25 14:52:01 -0400

    update molgpu01

--- tune.sh
+++ tune.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-model="smiles_ga"
+model="smiles_vae"
 
 nohup python -u run.py ${model} \
     --n_jobs 16 --task tune --wandb online \
@@ -10,3 +10,4 @@ nohup python -u run.py ${model} \
 #     --n_jobs 16 --task tune --n_runs 30 --wandb online \
 #     --oracles zaleplon_mpo perindopril_mpo > tune_${model}.out 2>&1 &
 
+# CUDA_VISIBLE_DEVICES=0
