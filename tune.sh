85b416bbec3911e0e041b22ebf5f0c7e87ac7ac7
wenhao-gao <gaowh19@gmail.com>
2022-04-26 11:26:51 -0400

    finish tune smiles/selfies lstm hc

--- tune.sh
+++ tune.sh
@@ -1,13 +1,9 @@
 #!/bin/bash 
 
-model="smiles_vae"
+model="smiles_lstm_hc"
 
 nohup python -u run.py ${model} \
-    --n_jobs 16 --task tune --wandb online \
+    --n_jobs 16 --task tune --n_runs 50 --wandb online \
     --oracles zaleplon_mpo perindopril_mpo > tune_${model}.out 2>&1 &
-
-# nohup python -u run.py ${model} \
-#     --n_jobs 16 --task tune --n_runs 30 --wandb online \
-#     --oracles zaleplon_mpo perindopril_mpo > tune_${model}.out 2>&1 &
 
 # CUDA_VISIBLE_DEVICES=0
