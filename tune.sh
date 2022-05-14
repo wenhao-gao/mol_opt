23e4b25955e57624e5f5b621c38e69e810d40851
wenhao-gao <gaowh19@gmail.com>
2022-05-13 23:07:36 -0400

    update MARS parameters and start to tune reinvent

--- tune.sh
+++ tune.sh
@@ -1,9 +1,9 @@
 #!/bin/bash 
 
-model="mars"
+model="reinvent"
 
 nohup python -u run.py ${model} \
-    --n_jobs 8 --task tune --n_runs 30 --wandb online \
+    --n_jobs 8 --task tune --n_runs 100 --wandb online \
     --oracles zaleplon_mpo perindopril_mpo > tune_${model}.out 2>&1 &
 
 # CUDA_VISIBLE_DEVICES=2
