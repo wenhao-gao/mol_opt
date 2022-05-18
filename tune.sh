83e68f5cb82302c3b83605d2bcbbcb14bb08d76f
wenhao-gao <gaowh19@gmail.com>
2022-05-18 11:07:00 -0400

    update mol1

--- tune.sh
+++ tune.sh
@@ -3,7 +3,7 @@
 model="mars"
 
 nohup python -u run.py ${model} \
-    --n_jobs 8 --task tune --n_runs 30 --wandb online \
+    --n_jobs 8 --task tune --n_runs 100 --wandb online \
     --oracles zaleplon_mpo perindopril_mpo > tune_${model}.out 2>&1 &
 
 # CUDA_VISIBLE_DEVICES=2
