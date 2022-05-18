d936defcd5d10df67a4fba40e76bef89a712d1fe
wenhao-gao <gaowh19@gmail.com>
2022-05-18 11:12:56 -0400

    update mol2

--- make_run_prod.sh
+++ make_run_prod.sh
@@ -14,7 +14,7 @@ oracle_array=('jnk3' 'gsk3b' 'celecoxib_rediscovery' \\
     'thiothixene_rediscovery' 'albuterol_similarity' 'mestranol_similarity' \\
     'isomers_c7h8n2o2' 'isomers_c9h10n2o2pf2cl' 'median1' 'median2' 'osimertinib_mpo' \\
     'fexofenadine_mpo' 'ranolazine_mpo' 'perindopril_mpo' 'amlodipine_mpo' \\
-    'sitagliptin_mpo' 'zaleplon_mpo' 'valsartan_smarts' 'deco_hop' 'scaffold_hop')
+    'sitagliptin_mpo' 'zaleplon_mpo' 'valsartan_smarts' 'deco_hop' 'scaffold_hop' 'qed' 'drd2')
 
 for oralce in "\${oracle_array[@]}"
 do
