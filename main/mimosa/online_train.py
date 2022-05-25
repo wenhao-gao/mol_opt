c1951b1b14ffa011ed133b15e76fd0114b4c1c24
wenhao-gao <gaowh19@gmail.com>
2022-05-25 13:21:59 -0400

    mimosa ready to tune

--- main/mimosa/online_train.py
+++ main/mimosa/online_train.py
@@ -15,22 +15,16 @@ def train_gnn(data, gnn, epoch=5):
 		data is smiles list 
 	"""
 	shuffle(data)
-	# train_data = data[:N]
-	# valid_data = data[N:]
-
 	training_set = Molecule_Dataset(data)
-	# valid_set = Molecule_Dataset(valid_data)
+
 	params = {'batch_size': 1,
 	          'shuffle': True,
 	          'num_workers': 1}
 	def collate_fn(batch_lst):
 		return batch_lst
 	train_generator = torch.utils.data.DataLoader(training_set, collate_fn = collate_fn, **params)
-	# valid_generator = torch.utils.data.DataLoader(valid_set, collate_fn = collate_fn, **params)
 
 	cost_lst = []
-	# valid_loss_lst = []
-	# every_k_iters = 5000
 
 	for ep in tqdm(range(epoch)):
 		for i, smiles in tqdm(enumerate(train_generator)):
@@ -47,34 +41,9 @@ def train_gnn(data, gnn, epoch=5):
 			node_mat = torch.FloatTensor(node_mat).to(device)
 			adjacency_matrix = torch.FloatTensor(adjacency_matrix).to(device)
 			label = torch.LongTensor([label]).view(-1).to(device)
-			# print('label', label)
 			cost = gnn.learn(node_mat, adjacency_matrix, idx, label)
 			cost_lst.append(cost)
 
-			# #### 2. validation 
-			# if i % every_k_iters == 0:
-			# 	gnn.eval()
-			# 	valid_loss, valid_num = 0,0 
-			# 	for smiles in valid_generator:
-			# 		smiles = smiles[0]
-			# 		try:
-			# 			graph_feature = smiles2feature(smiles)  
-			# 			if graph_feature is None:
-			# 				continue 
-			# 			node_mat, adjacency_matrix, idx, label = graph_feature 
-			# 		except:
-			# 			continue 
-			# 		node_mat = torch.FloatTensor(node_mat).to(device)
-			# 		adjacency_matrix = torch.FloatTensor(adjacency_matrix).to(device)
-			# 		label = torch.LongTensor([label]).view(-1).to(device)
-			# 		cost, _ = gnn.infer(node_mat, adjacency_matrix, idx, label)
-			# 		valid_loss += cost
-			# 		valid_num += 1 
-			# 	valid_loss = valid_loss / valid_num
-			# 	valid_loss_lst.append(valid_loss)
-			# 	# file_name = save_folder + str(ep) + "_validloss_" + str(valid_loss)[:7] + ".ckpt"
-			# 	# torch.save(gnn, file_name)
-			# 	gnn.train()
 	return gnn 
 
 
