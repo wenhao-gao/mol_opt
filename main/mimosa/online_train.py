46901e5d3c14565ec919240c0e269a00d5b7f447
wenhao-gao <gaowh19@gmail.com>
2022-05-25 13:17:37 -0400

    mimosa changed to hill-climbing

--- main/mimosa/online_train.py
+++ main/mimosa/online_train.py
@@ -10,30 +10,28 @@ from chemutils import smiles2feature
 from utils import Molecule_Dataset 
 device = 'cpu'
 
-def train_gnn(data, gnn): 
+def train_gnn(data, gnn, epoch=5): 
 	"""
 		data is smiles list 
 	"""
 	shuffle(data)
-	N = int(len(data) * 0.9)
-	train_data = data[:N]
-	valid_data = data[N:]
+	# train_data = data[:N]
+	# valid_data = data[N:]
 
-	training_set = Molecule_Dataset(train_data)
-	valid_set = Molecule_Dataset(valid_data)
+	training_set = Molecule_Dataset(data)
+	# valid_set = Molecule_Dataset(valid_data)
 	params = {'batch_size': 1,
 	          'shuffle': True,
 	          'num_workers': 1}
 	def collate_fn(batch_lst):
 		return batch_lst
 	train_generator = torch.utils.data.DataLoader(training_set, collate_fn = collate_fn, **params)
-	valid_generator = torch.utils.data.DataLoader(valid_set, collate_fn = collate_fn, **params)
+	# valid_generator = torch.utils.data.DataLoader(valid_set, collate_fn = collate_fn, **params)
 
 	cost_lst = []
-	valid_loss_lst = []
-	epoch = 5 
-	every_k_iters = 5000
-	# save_folder = "save_model/GNN_epoch_" 
+	# valid_loss_lst = []
+	# every_k_iters = 5000
+
 	for ep in tqdm(range(epoch)):
 		for i, smiles in tqdm(enumerate(train_generator)):
 			### 1. training
@@ -53,30 +51,30 @@ def train_gnn(data, gnn):
 			cost = gnn.learn(node_mat, adjacency_matrix, idx, label)
 			cost_lst.append(cost)
 
-			#### 2. validation 
-			if i % every_k_iters == 0:
-				gnn.eval()
-				valid_loss, valid_num = 0,0 
-				for smiles in valid_generator:
-					smiles = smiles[0]
-					try:
-						graph_feature = smiles2feature(smiles)  
-						if graph_feature is None:
-							continue 
-						node_mat, adjacency_matrix, idx, label = graph_feature 
-					except:
-						continue 
-					node_mat = torch.FloatTensor(node_mat).to(device)
-					adjacency_matrix = torch.FloatTensor(adjacency_matrix).to(device)
-					label = torch.LongTensor([label]).view(-1).to(device)
-					cost, _ = gnn.infer(node_mat, adjacency_matrix, idx, label)
-					valid_loss += cost
-					valid_num += 1 
-				valid_loss = valid_loss / valid_num
-				valid_loss_lst.append(valid_loss)
-				# file_name = save_folder + str(ep) + "_validloss_" + str(valid_loss)[:7] + ".ckpt"
-				# torch.save(gnn, file_name)
-				gnn.train()
+			# #### 2. validation 
+			# if i % every_k_iters == 0:
+			# 	gnn.eval()
+			# 	valid_loss, valid_num = 0,0 
+			# 	for smiles in valid_generator:
+			# 		smiles = smiles[0]
+			# 		try:
+			# 			graph_feature = smiles2feature(smiles)  
+			# 			if graph_feature is None:
+			# 				continue 
+			# 			node_mat, adjacency_matrix, idx, label = graph_feature 
+			# 		except:
+			# 			continue 
+			# 		node_mat = torch.FloatTensor(node_mat).to(device)
+			# 		adjacency_matrix = torch.FloatTensor(adjacency_matrix).to(device)
+			# 		label = torch.LongTensor([label]).view(-1).to(device)
+			# 		cost, _ = gnn.infer(node_mat, adjacency_matrix, idx, label)
+			# 		valid_loss += cost
+			# 		valid_num += 1 
+			# 	valid_loss = valid_loss / valid_num
+			# 	valid_loss_lst.append(valid_loss)
+			# 	# file_name = save_folder + str(ep) + "_validloss_" + str(valid_loss)[:7] + ".ckpt"
+			# 	# torch.save(gnn, file_name)
+			# 	gnn.train()
 	return gnn 
 
 
--- main/mimosa/run.py
+++ main/mimosa/run.py
@@ -6,8 +6,7 @@ sys.path.append('.')
 from main.optimizer import BaseOptimizer
 from chemutils import * 
 from inference_utils import * 
-from online_train import * ### 
-# def train_gnn(data, gnn): 
+from online_train import * 
 from random import shuffle 
 class MIMOSA_Optimizer(BaseOptimizer):
 
@@ -29,6 +28,7 @@ class MIMOSA_Optimizer(BaseOptimizer):
 
 		patience = 0
 		all_smiles_score_list = []  
+
 		while True:
 
 			if len(self.oracle) > 100:
@@ -38,14 +38,14 @@ class MIMOSA_Optimizer(BaseOptimizer):
 				old_scores = 0
 
 			next_set = set()
-			# print('Selecting ')
 			for smiles in current_set:
 				smiles_set = optimize_single_molecule_one_iterate(smiles, gnn)
 				next_set = next_set.union(smiles_set)
 
 			smiles_lst = list(next_set)
 			shuffle(smiles_lst)
-			smiles_lst = smiles_lst[:500]
+			print(f"Generated molecules: {len(smiles_lst)}")
+			smiles_lst = smiles_lst[:config['offspring_size']]
 
 			score_lst = self.oracle(smiles_lst)
 
@@ -60,11 +60,14 @@ class MIMOSA_Optimizer(BaseOptimizer):
 			### online train gnn 
 			all_smiles_score_list.extend(smiles_score_lst)
 			all_smiles_score_list.sort(key=lambda x:x[1], reverse=True)
-			good_smiles_list = [i[0] for i in filter(lambda x:x[1] > 0.5, all_smiles_score_list)]
-			if len(good_smiles_list) < 300:
-				good_smiles_list.extend(all_smiles_score_list[:300])
-			train_gnn(good_smiles_list, gnn)
+			# good_smiles_list = [i[0] for i in filter(lambda x:x[1] > 0.5, all_smiles_score_list)]
+			# if len(good_smiles_list) < config['train_data_size']:
+			# 	good_smiles_list.extend(all_smiles_score_list[:config['train_data_size']])
 
+			# import ipdb; ipdb.set_trace()
+			# print(f"Training mol length: {len(good_smiles_list)}")
+			good_smiles_list = all_smiles_score_list[:config['train_data_size']]
+			train_gnn(good_smiles_list, gnn, epoch=config['train_epoch'])
 
 			# early stopping
 			if len(self.oracle) > 2000:
@@ -72,12 +75,10 @@ class MIMOSA_Optimizer(BaseOptimizer):
 				new_scores = [item[1][0] for item in list(self.mol_buffer.items())[:100]]
 				if new_scores == old_scores:
 					patience += 1
-					if patience >= self.args.patience * 5:
+					if patience >= self.args.patience:
 						self.log_intermediate(finish=True)
 						print('convergence criteria met, abort ...... ')
 						break
 				else:
 					patience = 0
-
-
 
