cfb81c9470e0a83b22f1a0f1d7c19e62f187a195
wenhao-gao <gaowh19@gmail.com>
2022-05-31 09:52:17 -0400

    tune mimosa finish

--- tune.sh
+++ tune.sh
@@ -6,4 +6,4 @@ nohup python -u run.py ${model} \
     --n_jobs 16 --task tune --n_runs 100 --wandb online \
     --oracles zaleplon_mpo perindopril_mpo > tune_${model}.out 2>&1 &
 
-# CUDA_VISIBLE_DEVICES=0
+# CUDA_VISIBLE_DEVICES=1 bash tune.sh
