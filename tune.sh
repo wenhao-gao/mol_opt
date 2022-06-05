142d218886044dd5e996b7d2991de41ae118811c
wenhao-gao <gaowh19@gmail.com>
2022-06-05 00:06:43 -0400

    GFlowNet AL tune finish

--- tune.sh
+++ tune.sh
@@ -1,9 +1,9 @@
 #!/bin/bash 
 
-model="stoned"
+model="smiles_ga"
 
 nohup python -u run.py ${model} \
-    --n_jobs 16 --task tune --n_runs 20 --wandb online \
+    --n_jobs 16 --task tune --n_runs 50 --wandb online \
     --oracles zaleplon_mpo perindopril_mpo > tune_${model}.out 2>&1 &
 
 # CUDA_VISIBLE_DEVICES=1 bash tune.sh
